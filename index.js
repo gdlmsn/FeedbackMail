8612751ea3437e2f2edbf207fd6ccaa9ac647ee4
Giulio <gdalmasson@gmail.com>
2021-03-10 20:21:21 +1100

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send({hi:'there'})
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
 });
 
-app.listen(5000);
-
+//env variable for heroku runtime config
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
